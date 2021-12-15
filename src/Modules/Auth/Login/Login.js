11e0cd78acca9c8d989bb96c1e2b4480cebb2dd3
priya lakshmi <lekhapriya98@gmail.com>
2021-12-15 11:32:03 +0530

    login page validation completed

--- src/Modules/Auth/Login/Login.js
+++ src/Modules/Auth/Login/Login.js
@@ -3,17 +3,15 @@ import { Row, Col, Input, Button } from "antd";
 import { useNavigate } from 'react-router';
 import useStateWithValidation from "../../Utils/useStateWithValidation";
 import Logo from "../../../assets/image/png/Logo.png";
-import { login } from "../../../Actions/Auth"
-import { connect } from "react-redux";
 import "../../sass/style.css";
 
-const Login = ({ login, isAuthenticated }) => {
+const Login = () => {
     let navigate = useNavigate();
     const [
         userEmailInput,
         setUserEmailInput,
         validEmail,
-    ] = useStateWithValidation((email) => email.length > 3, "");
+    ] = useStateWithValidation((email) => email.length > 5, "");
 
     const [checkValidation, setCheckValidation] = useState(false);
 
@@ -21,40 +19,71 @@ const Login = ({ login, isAuthenticated }) => {
         userpasswordInput,
         setUserPasswordInput,
         validPassword,
-    ] = useStateWithValidation((password) => password.length > 1, "");
+    ] = useStateWithValidation((password) => password.length >= 6, "");
 
     const [logininputs, setLoginInputs] = useState({});
 
+    const [passwordRequired, setpasswordRequired] = useState(false);
+    const [emailRequired, setEmailRequired] = useState(false);
+    const [checkEmailVaidation, setCheckEmailVaildation] = useState(false);
+
     const onChangeEmailInput = (e) => {
         setUserEmailInput(e.target.value);
+        setEmailRequired(false);
+        setCheckEmailVaildation(false);
     };
 
     const onChangePasswordInput = (e) => {
         setUserPasswordInput(e.target.value);
+        setpasswordRequired(false);
     };
 
-
-
+    const validate = () => {
+        if (userpasswordInput === "" || userpasswordInput === undefined) {
+            setpasswordRequired(true);
+        }
+        if (userEmailInput === "" || userEmailInput === undefined) {
+            setEmailRequired(true);
+        }
+        if (!userpasswordInput && !userEmailInput) {
+            return false;
+        }
+        return true;
+    }
 
     const handleSubmit = async (event) => {
         event.preventDefault();
-        setCheckValidation(true);
 
-        try {
-            let credentials = await login({
-                email: userEmailInput,
-                password: userpasswordInput,
-            });
-            console.log("login", login.state);
+        if (validate()) {
+            setCheckValidation(true);
+            setpasswordRequired(false);
+            setEmailRequired(false);
+            try {
+                const checkEmail = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-            // if (credentials) {
-            //     navigate("/managesite");
-            // }
-        } catch (error) {
-            console.log("error", error);
+                if (checkEmail.test(String(userEmailInput).toLowerCase()) === false) {
+                    setCheckEmailVaildation(true);
+                }
+
+                else {
+                    let credentials = {
+                        email: userEmailInput,
+                        password: userpasswordInput,
+                    };
+                    setLoginInputs(credentials);
+                    console.log("login", credentials);
+                    console.log("email check", checkEmail.test(String(userEmailInput).toLowerCase()));
+                    console.log("password", validPassword.toString());
+
+                    if (validEmail.toString() == "true" && validPassword.toString() == "true") {
+                        navigate("/home");
+                    }
+                }
+            } catch (error) {
+                console.log("error", error);
+            }
         }
     };
-    console.log("isAuthenticated", isAuthenticated);
 
     return (
         <React.Fragment>
@@ -75,7 +104,7 @@ const Login = ({ login, isAuthenticated }) => {
                                 <img src={Logo} alt="logo" />
                             </div>
                             <div className="pt-6" >
-                            <h1 >Site Cloud</h1></div>
+                                <h1 >Site Cloud</h1></div>
                             <div className="login_form">
                                 <h2>Login</h2>
                                 <form onSubmit={handleSubmit}>
@@ -85,21 +114,35 @@ const Login = ({ login, isAuthenticated }) => {
 
                                         <label className="form_label ">Email Address</label>
                                         <div className="pt-1">
-                                        <Input
-                                            placeholder="&nbsp;&nbsp;&nbsp;danielrich@gmail.com"
-                                            name="userEmailInput"
-                                            className="login_input_text"
-                                            value={userEmailInput}
-                                            onChange={onChangeEmailInput} 
+                                            <Input
+                                                placeholder="&nbsp;&nbsp;&nbsp;danielrich@gmail.com"
+                                                name="userEmailInput"
+                                                className="login_input_text"
+                                                value={userEmailInput}
+                                                onChange={onChangeEmailInput}
                                             /></div>
-
-
+                                        {/* required mail validation */}
+                                        {
+                                            emailRequired && (
+                                                <>
+                                                    <span className="validation_error text-focus-in">*Email ID is required</span>
+                                                </>
+                                            )
+                                        }
                                         {/* email validation */}
                                         {checkValidation === true && validEmail === false && (
                                             <>
-                                                <span className="validation_error text-focus-in">*Email is required</span>
+                                                <span className="validation_error text-focus-in">*Email ID is required</span>
                                             </>
                                         )}
+                                        {/* Correct mail ID validation */}
+                                        {
+                                            checkEmailVaidation && (
+                                                <>
+                                                    <span className="validation_error text-focus-in">*Email is not valid</span>
+                                                </>
+                                            )
+                                        }
                                     </div>
 
                                     {/* Email field ends */}
@@ -118,12 +161,20 @@ const Login = ({ login, isAuthenticated }) => {
                                             />
                                         </div>
                                         {/* password validation */}
-                                        {checkValidation === true && validPassword === false && (
-                                            <>
-                                                <span className="validation_error text-focus-in">*Password is required</span>
-                                            </>
-                                        )}
+                                        {passwordRequired
+                                            && (
+                                                <>
+                                                    <span className="validation_error text-focus-in">*Password is required</span>
+                                                </>
+                                            )}
 
+                                        {/* passord length validation */}
+                                        {
+                                            checkValidation === true && validPassword === false && (
+                                                <>
+                                                    <span className="validation_error text-focus-in">*Password must have minimum 6 characters</span>
+                                                </>
+                                            )}
                                     </div>
                                     {/* Password field ends */}
 
@@ -134,7 +185,7 @@ const Login = ({ login, isAuthenticated }) => {
                                     </div>
 
                                     <div className="login_btn">
-                                        <button type="submit" onClick={() => { navigate("/home") }}  className="login_button_text" >
+                                        <button type="submit" className="login_button_text" >
                                             Login
                                         </button>
                                     </div>
@@ -149,8 +200,5 @@ const Login = ({ login, isAuthenticated }) => {
     )
 }
 
+export default Login;
 
-const mapStateToProps = (state) => ({
-    isAuthenticated: state.Auth.isAuthenticated,
-});
-export default connect(mapStateToProps, { login })(Login);
